8852de3211845f2f2742aa110cb13d484ee4e9f2
varshithno1 <luckyvarshith24@gmail.com>
2024-06-30 19:11:45 +0530

    Fixed Deployment errors on Logout api route

--- src/app/api/user/logout/route.tsx
+++ src/app/api/user/logout/route.tsx
@@ -1,15 +1,16 @@
 import { logout } from "@/lib/jwt";
+import { NextResponse } from "next/server";
 
 export async function POST() {
     try {
       await logout();
     } catch (error) {
       console.error('Error deleting cookie:', error);
-      return {
+      return NextResponse.json({
         statusCode: 500, // Internal Server Error
         body: JSON.stringify({ error: 'Failed to log out' }),
-      };
+      });
     }
-    return null; // Or return an empty object
+    return null;
   }
   

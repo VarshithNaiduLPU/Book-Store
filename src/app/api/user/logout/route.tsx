a4650bf2c2e710f05dc27cb61c8a4bef3a45cdaf
varshithno1 <luckyvarshith24@gmail.com>
2024-06-30 19:15:13 +0530

    Deploy Test

--- src/app/api/user/logout/route.tsx
+++ src/app/api/user/logout/route.tsx
@@ -1,15 +1,11 @@
 import { logout } from "@/lib/jwt";
-import { NextResponse } from "next/server";
 
 export async function POST() {
     try {
       await logout();
     } catch (error) {
       console.error('Error deleting cookie:', error);
-      return NextResponse.json({
-        statusCode: 500, // Internal Server Error
-        body: JSON.stringify({ error: 'Failed to log out' }),
-      });
+      return;
     }
     return null;
   }
